0826f96f4fd32f682879b4cb8b52df48715ff5d3
Dmitry Komkov <drnecromant@gmail.com>
2017-12-16 15:29:24 +0100

    Declare http module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes.component';
 
+
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, NotesComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+  ],
+  declarations: [
+    AppComponent,
+    NotesComponent,
+  ],
   bootstrap: [ AppComponent ]
 })
 
