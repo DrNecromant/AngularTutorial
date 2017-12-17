8deb80ad989bfcf2a8bce443413f0815b2167b28
Dmitry Komkov <drnecromant@gmail.com>
2017-12-17 21:24:50 +0100

    Added routing for main application and others

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes.component';
 import { SectionsComponent } from './sections.component';
 
+const appRoutes: Routes = [
+  { path: '', component: NotesEditorComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   declarations: [
     AppComponent,
