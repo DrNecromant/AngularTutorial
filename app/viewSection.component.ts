ebf3050edecb43a2f7fa79794e961dbe89c36d10
Dmitry Komkov <drnecromant@gmail.com>
2017-12-17 23:08:41 +0100

    Added notes to viewSection component

--- app/viewSection.component.ts
+++ app/viewSection.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { NotesService } from './services/api/notes.service';
+import { Note } from './interfaces'
+
 @Component({
     selector: 'view-section',
     templateUrl: 'app/viewSection.component.html',
@@ -8,10 +11,16 @@ import { ActivatedRoute } from '@angular/router';
 
 export class ViewSectionComponent implements OnInit {
   sectiontext: string;
+  notes: Note[];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private notesService: NotesService) {}
 
-  ngOnInit() {  
+  ngOnInit() {
     this.sectiontext = this.route.snapshot.params["name"];
+    this.getNotes().subscribe(notes => { this.notes = notes });
+  }
+
+  getNotes() {
+    return this.notesService.getNotes(this.sectiontext);
   }
 }
