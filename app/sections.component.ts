65edd9db2b686fd6d07156a6181e78c5f2edfbbf
Dmitry Komkov <drnecromant@gmail.com>
2017-12-17 11:47:38 +0100

    Add ablitity to read seaction on client side
    Also add some style fixes

--- app/sections.component.ts
+++ app/sections.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
+import { Http, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
 
 interface Section {
-    _id: string;
-    title: string;
+  _id?: string;
+  title: string;
 }
 
 @Component({
-    selector: 'sections',
-    templateUrl: 'app/sections.component.html',
+  selector: 'sections',
+  templateUrl: 'app/sections.component.html',
 })
 
 export class SectionsComponent {
   private sectionsUrl = 'sections';
   sections: Section[];
+
+  constructor(private http: Http) {
+      this.readSections();
+  }
+
+  readSections() {
+    this.getSections().subscribe(sections=> {
+        this.sections = sections;
+    });
+  }
+
+  getSections(): Observable<Section[]> {
+    return this.http.get(this.sectionsUrl)
+      .map(response => response.json() as Section[]);
+  }
 }
