f01bba1ffe167e29d97351ba4dbe8f551a252262
Dmitry Komkov <drnecromant@gmail.com>
2017-12-20 19:13:43 +0100

    Added basic form validation
    added required and patterns

--- app/registration/userForm.component.ts
+++ app/registration/userForm.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 import { User } from '../interfaces';
 
 @Component({
-    templateUrl: 'app/registration/userForm.component.html',
+  templateUrl: 'app/registration/userForm.component.html',
+  styles: [`
+    input.ng-touched.ng-invalid {
+      background-color: #ffe8f1;
+    }
+  `]
 })
 
 export class UserFormComponent {
