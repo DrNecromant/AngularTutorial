4399e1f548933966417544da94706897018f579a
Dmitry Komkov <drnecromant@gmail.com>
2017-12-23 15:07:00 +0100

    Return UserLogin instead of string in api

--- server/server.js
+++ server/server.js
@@ -123,7 +123,7 @@ app.post("/users", function(req, res) {
 });
 
 app.get("/user", function(req, res) {
-  res.send(req.session.userName);
+  res.send({ userName: req.session.userName })
 });
 
 // ==========
