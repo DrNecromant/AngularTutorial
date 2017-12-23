e5a68401c5d8e6dd834e2f172b3f5f2f4fd099b7
Dmitry Komkov <drnecromant@gmail.com>
2017-12-23 01:11:21 +0100

    Added authorization service

--- app/services/api/login.service.ts
+++ app/services/api/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 
 import { LoginUser } from '../../interfaces'
 
@@ -13,4 +14,14 @@ export class LoginService {
 
   constructor(private http: Http) { }
 
+  login(user: LoginUser): Observable<boolean> {
+    return this.http.post(this.loginUrl, user)
+      .map(response => response.json() as boolean)
+      .do(res => { if (res) this.loggedIn = true });
+  }
+
+  logout() {
+    return this.http.get(this.logoutUrl)
+      .do(res => this.loggedIn = false);
+  }
 }
