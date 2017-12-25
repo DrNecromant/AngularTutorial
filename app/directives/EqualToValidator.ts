cf91a9be5615a877484c488a9775298a2f9255b6
Dmitry Komkov <drnecromant@gmail.com>
2017-12-25 19:32:21 +0100

    Use Observable instead of Promise in validator

--- app/directives/EqualToValidator.ts
+++ app/directives/EqualToValidator.ts
@@ -14,13 +14,13 @@ import { Subscription } from 'rxjs/Subscription';
 export class EqualToValidator implements Validator {
   subscription: Subscription;
 
-  constructor( @Attribute("validateEqual") public validateEqual: string) { }
+  constructor(@Attribute("validateEqual") public validateEqual: string) { }
 
-  validate(c: AbstractControl): {[key: string]: any} {
+  validate(c: AbstractControl): { [key: string]: any } {
     let v = c.value;  // Get current element value
     let e = c.root.get(this.validateEqual);  // Find html element to compare
-    this.subscription = e.valueChanges.subscribe((val:string)=> {
-        if (val != v) c.setErrors({validateEqual: false});
+    this.subscription = e.valueChanges.subscribe((val: string)=> {
+        if (val != v) c.setErrors({ validateEqual: false });
         else c.setErrors(null);
     });
     if (e && v !== e.value) return { validateEqual: false };
--- app/directives/UserUniqueValidator.ts
+++ app/directives/UserUniqueValidator.ts
@@ -1,27 +1,26 @@
 import { Directive } from '@angular/core';
 import { Validator, NG_ASYNC_VALIDATORS, AbstractControl } from '@angular/forms';
 import { Http, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Directive({
   selector: '[userUniqueValid][ngModel]',
   providers: [{
-    provide:NG_ASYNC_VALIDATORS,
+    provide: NG_ASYNC_VALIDATORS,
     useExisting: UserUniqueValidator,
     multi: true,
   }]
 })
 
 export class UserUniqueValidator implements Validator {
-  constructor( private http: Http) { }
+  constructor(private http: Http) { }
 
-  validate(c: AbstractControl): Promise<{[key: string]: any}> {
+  validate(c: AbstractControl): Observable<{ [key: string]: any }> {
     const user = c.value;
     const params: URLSearchParams = new URLSearchParams();
     params.set('user', user);
 
-    return new Promise(resolve =>
-      this.http.get("checkUserUnique", {search:params})
-        .map(response => response.json())
-        .subscribe(res => res?resolve(null):resolve({userUniqueValid: false})));
+    return this.http.get("checkUserUnique", { search: params })
+      .map(response => response.json()? null:{ userUniqueValid: false });
   }
 }
