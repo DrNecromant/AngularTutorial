f560b760ee34061683ebbb69dc0c99a8f273ffed
Dmitry Komkov <drnecromant@gmail.com>
2017-12-26 22:50:27 +0100

    Added ability to chose country and town on user registration

--- app/forms/userForm.component.ts
+++ app/forms/userForm.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+import { Component, OnInit } from '@angular/core';
+import { Http, URLSearchParams } from '@angular/http';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import { LoginService } from '../services/api/login.service'
 
@@ -15,15 +17,40 @@ import { User } from '../interfaces';
   `]
 })
 
-export class UserFormComponent {
+export class UserFormComponent implements OnInit {
   user: User = new User;
   minAge: string = '18';
+  countries$: Observable<string[]>;
+  towns$: Observable<string[]>;
 
   constructor(
     private http: Http,
     private router: Router,
     private loginService: LoginService,
   ) { }
+
+  ngOnInit() {
+    this.countries$ = this.getCountries();
+    this.countries$.subscribe(countries => {
+      if (countries) this.towns$ = this.getTowns(countries[0]);
+    });
+  }
+
+  changeCountry(country: string) {
+    this.towns$ = this.getTowns(country);
+  }
+
+  getCountries(): Observable<string[]> {
+    return this.http.get('countries')
+      .map(response => response.json() as string[]);
+  }
+
+  getTowns(country: string): Observable<string[]> {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('country', country);
+    return this.http.get('towns', { search: params })
+      .map(response => response.json() as string[]);
+  }
 
   onSubmit() {
     this.http.post("users", this.user).subscribe(res => {
